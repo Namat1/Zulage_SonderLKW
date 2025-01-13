53d345827277475bb3e374ee2e4b8ec7771a53bb
Martin <mcarstensen79@gmail.com>
2025-01-13 08:48:10 +0100

    Update sonderzulage_berechnung.py

--- sonderzulage_berechnung.py
+++ sonderzulage_berechnung.py
@@ -255,8 +255,9 @@ def add_summary(sheet, summary_data, start_col=9, month_name=""):
         name_cell.fill = name_fill
         name_cell.border = thin_border
 
-        personalnummer_cell = sheet.cell(row=i, column=start_col + 1, value=f"'{personalnummer}")
-        personalnummer_cell.number_format = '@'  # Textformatierung, um führende Nullen zu erhalten
+    for row in range(4, len(summary_data) + 4):  # Startzeile und Länge der Daten
+        personalnummer_cell = sheet.cell(row=row, column=start_col + 1)
+        personalnummer_cell.number_format = '00000000'
         personalnummer_cell.font = Font(bold=True, size=12)
         personalnummer_cell.alignment = Alignment(horizontal="right", vertical="center")
         personalnummer_cell.fill = personalnummer_fill
